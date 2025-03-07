d38e0fed0ff5ac430c6d53502e99403413c54e8f
RohitSharma2410 <68576284+RohitSharma2410@users.noreply.github.com>
2025-03-07 14:38:25 +0530

    Added casted apache http client for handling timeouts

--- src/test/java/engine/Hooks.java
+++ src/test/java/engine/Hooks.java
@@ -3,10 +3,19 @@ package engine;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.net.HttpRetryException;
+import java.net.http.HttpClient;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.hamcrest.Matchers;
 
 import io.cucumber.java.Before;
@@ -14,9 +23,12 @@ import io.restassured.RestAssured;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.config.ConnectionConfig;
 import io.restassured.config.HttpClientConfig;
+import io.restassured.config.HttpClientConfig.HttpClientFactory;
 import io.restassured.config.RestAssuredConfig;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.internal.RestAssuredHttpBuilderGroovyHelper;
+import io.restassured.internal.http.HTTPBuilder;
 import io.restassured.specification.RequestSpecification;
 
 public class Hooks {
@@ -27,21 +39,36 @@ public class Hooks {
 	@Before
 	public static void before() {
 		try {
+		//Creating Request Config object
+			RequestConfig requestConfig = RequestConfig.custom()
+	                .setConnectTimeout(5000) 
+	                .setSocketTimeout(5000) 
+	                .build();
+
+	        //Create http client object and passing request config object 
+	        CloseableHttpClient httpClient = HttpClients.custom()
+	                .setDefaultRequestConfig(requestConfig)
+	                .setConnectionManager(new BasicHttpClientConnectionManager()) // Connection pooling
+	                .build();
+	        
+	        //Create rest assured config object that can be passed to rest assured config method
+	        RestAssuredConfig config = RestAssuredConfig.config()
+	                .httpClient(HttpClientConfig.httpClientConfig()
+	                        .setParam("http.client", httpClient) // Pass the custom HttpClient here
+	                );
+      
+	     
+	        
+
 //Setting up ResponseSpecBuilder to test response at run time in step definition class
 			res = new ResponseSpecBuilder();
 			res.expectStatusCode(Matchers.lessThan(500));
 //Added custom http client config and Connection config and bind it with RestAssured to handle to timeout cases			
 			
-			Map<String, Object> addmap = new HashMap<>();
-			addmap.put("http.connection.timeout", 10000);
-			HttpClientConfig httpConfig = HttpClientConfig.httpClientConfig().addParams(addmap);
-			Map<String, Object> config = new HashMap<>();
-			config.put("http.connection.timeout", 10000);
-			ConnectionConfig conconfig = ConnectionConfig.connectionConfig()
-					.closeIdleConnectionsAfterEachResponseAfter(10000, TimeUnit.SECONDS);
+		
+		
 			
 	
-			RestAssuredConfig con = RestAssured.config.httpClient(httpConfig).connectionConfig(conconfig);
 	//Added filters to log request to LogsFiles
 			FileOutputStream requestLogFile = new FileOutputStream(
 					System.getProperty("user.dir") + "/logs/requestLogs.log", true); // 'true' for appending
@@ -49,7 +76,7 @@ public class Hooks {
 					System.getProperty("user.dir") + "/logs/responseLogs.log", true); // 'true' for appending
 		
 	//Built base request specification to be used further for request		
-			requestSpec = RestAssured.given().config(con).contentType("application/json")
+			requestSpec = RestAssured.given().config(config).contentType("application/json")
 					.filter(RequestLoggingFilter.logRequestTo(new PrintStream(requestLogFile)))
 					.filter(ResponseLoggingFilter.logResponseTo(new PrintStream(responseLogFile)))
 					.urlEncodingEnabled(false).log().all();
